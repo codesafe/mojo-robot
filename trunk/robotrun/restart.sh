6ffc4c93b77c5edcf377343c0ee8b696935f9323
codesafe <codesafe@gmail.com>
2016-09-04 00:39:45 +0900

    .

--- trunk/robotrun/restart.sh
+++ trunk/robotrun/restart.sh
@@ -3,13 +3,13 @@ sleep 1
 
 while [ 1 ] 
 do
-  PC=`ps -ex|grep robotrun|grep -v grep | awk '{print $1}'`
-  if [ "$PC" == "" ]             # 실행 프로세스가 없으면
-  then
-    CMD=`./robotrun 192.168.1.112 &`
-	break
-  fi
-  sleep 1
+	CNT=$(ps -ef|grep robotrun|grep -v grep|wc -l)
+	if [ $CNT -le 0 ]
+	then
+		CMD='./robotrun 192.168.1.112 &'
+		break
+	fi
+	sleep 1
 done
 
 
