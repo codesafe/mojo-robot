e9491aeb8862273c9f1ea519cfb6f45096c15462
codesafe <codesafe@gmail.com>
2016-09-04 00:20:11 +0900

    .

--- trunk/robotrun/restart.sh
+++ trunk/robotrun/restart.sh
@@ -1,3 +1,15 @@
 echo "Restart Robot"
-sleep 3
-./robotrun
+sleep 1
+
+while [ 1 ] 
+do
+  PC=`ps -ex|grep robotrun|grep -v grep | awk '{print $1}'`
+  if [ "$PC" == "" ]             # 실행 프로세스가 없으면
+  then
+    CMD=`./robotrun 192.168.1.112 &`
+	break
+  fi
+  sleep 1
+done
+
+
